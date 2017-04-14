2d3698adb0f7d5ffbb463a9a28a250f0b58febf5
Vlad Marțian <vlad.martian@raal.ro>
2017-04-14 11:28:34 +0300

    get heroes for dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
+    <nav>
     <a routerLink = "/dashboard">Dashboard</a>
     <a routerLink ="/heroes">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
         `
 })
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -64,9 +64,7 @@ import { HeroService } from './hero.service';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-    `],
-    providers:[HeroService]
-
+    `]
 })
 export class HeroesComponent implements OnInit {
   constructor(private heroService:HeroService){
