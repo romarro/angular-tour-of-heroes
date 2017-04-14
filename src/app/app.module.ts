54e252b5744783babc85cbc847dd47111a53f91b
Vlad Marțian <vlad.martian@raal.ro>
2017-04-14 09:05:44 +0300

    Added routing for HeroesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule } from '@angular/router';
+
 import { HeroDetailComponent } from'./hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 import { AppComponent }  from './app.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    RouterModule.forRoot([
+      {
+        path : 'heroes',
+        component : HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers : [HeroService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
