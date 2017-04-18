b1d1606a40653f562d8bbf2535c18ad113de36d6
Vlad Marțian <vlad.martian@raal.ro>
2017-04-18 14:56:57 +0300

    Added a html service with update and create. Create do not work. Heroes.component.html has problems.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { RouterModule } from '@angular/router';
 
+import { HttpModule } from "@angular/http";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
+
 import { HeroDetailComponent } from'./hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
 import { AppComponent }  from './app.component';
-import { AppRoutingModule } from "./app-routing.module";
+
 
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -29,4 +29,13 @@ export class HeroesComponent implements OnInit {
   ngOnInit():void{
     this.getHeroes();
   }
+  add(name:string):void{
+    name = name.trim();
+    if(!name){return;}
+    this.heroService.create(name)
+                    .then(hero =>{ 
+                      this.heroes.push(hero);
+                      this.selectedHero = null;
+                    });
+  }
 }
