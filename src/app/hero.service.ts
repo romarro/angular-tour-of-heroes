17bf2d0ad4f95dd4dffb1af00daff9e1a2c97cb3
Vlad Marțian <vlad.martian@raal.ro>
2017-04-19 14:26:37 +0300

    Added delete functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -47,4 +47,11 @@ export class HeroService{
             .then(res => res.json().data as Hero)
             .catch(this.handleError);
     }
+    delete(id:number):Promise<void>{
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url,{headers:this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
 }
